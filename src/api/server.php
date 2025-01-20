3505735e99395c195e0b91edbb3778759da1940f
gitName <bartk@list.ru>
2025-01-20 23:03:46 +0300

    feat: add getList server handler

---
+++ src/api/server.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-Type: application/json");
+
+try {
+    include "./../config/db.php";
+
+    $json = file_get_contents("php://input");
+    $data = json_decode($json, true);
+
+    if ($_GET["action"] === "getList" && $_SERVER["REQUEST_METHOD"] === "GET") {
+        $stmt = $conn->prepare("SELECT list FROM english WHERE id = :id");
+        $stmt->bindParam(":id", $data["id"]);
+        $stmt->execute();
+        $list = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($list);
+    }
+
+    if ($_GET["action"] === "addList" && $_SERVER["REQUEST_METHOD"] === "POST") {
+    }
+
+    if ($_GET["action"] === "editList" && $_SERVER["REQUEST_METHOD"] === "PUT") {
+    }
+
+    if ($_GET["action"] === "deleteList" && $_SERVER["REQUEST_METHOD"] === "DELETE") {
+    }
+} catch (PDOException $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
