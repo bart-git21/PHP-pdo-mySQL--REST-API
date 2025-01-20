d3308679c3c2089c2a1cd9322069ddab5f0f0e95
gitName <bartk@list.ru>
2025-01-20 23:59:55 +0300

    feat: add client selection addList event listener

--- public/js/index.js
+++ public/js/index.js
@@ -15,9 +15,25 @@ $(document).ready(function () {
         $("#textarea").val(response.list);
       })
       .fail(() => {})
-      .finally(() => {});
+      .always(() => {});
   });
   $("#saveList").on("click", function () {});
-  $("#createList").on("click", function () {});
+  $("#createList").on("click", function () {
+    const list = $("#textarea").val();
+    $.ajax({
+      url: "./../src/api/server.php?action=addList",
+      method: "POST",
+      contentType: "application/json",
+      headers: {
+        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+      },
+      data: JSON.stringify({ list }),
+    })
+      .done((response) => {
+        console.log(response);
+      })
+      .fail(() => {})
+      .always(() => {});
+  });
   $("#deleteList").on("click", function () {});
 });
