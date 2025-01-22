044c02d530118fac333fda2f72148bcc6fdc9dd5
gitName <bartk@list.ru>
2025-01-22 20:17:08 +0300

    refactor: add the user manual

--- public/pages/translate.php
+++ public/pages/translate.php
@@ -11,6 +11,21 @@
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     </div>
     <div class="card-footer text-body-secondary">
-        2 days ago
+        <div class="text-light">
+            <h4>Keyboard buttons control:</h4>
+            <div>
+                <i class="fa-solid fa-square-caret-left"></i> - return
+                last card
+            </div>
+            <div>
+                <i class="fa-solid fa-square-caret-right"></i> - call next
+                card
+            </div>
+            <div>
+                <i class="fa-solid fa-square-caret-up"></i><i class="fa-solid fa-square-caret-down"></i> - To
+                increase or decrease the delay time by 0.5 second
+            </div>
+            <div>[Spacebar] - start from begining</div>
+        </div>
     </div>
 </div>
