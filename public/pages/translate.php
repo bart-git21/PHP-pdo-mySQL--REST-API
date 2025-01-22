c0d95b79058fc5f2ed4c46dd214b86fedb2e8006
gitName <bartk@list.ru>
2025-01-22 21:03:34 +0300

    feat: add the progress bar

--- public/pages/translate.php
+++ public/pages/translate.php
@@ -5,10 +5,16 @@
             <button type="button" class="btn btn-primary">Continue (right arrow)</button>
             <button type="button" class="btn btn-danger">Stop</button>
         </div>
+        <input title="timer" type="email" class="form-control d-inline-block p-0 w-auto" id="translate_delay"
+            placeholder="delay" value="10">
     </div>
     <div class="card-body d-flex flex-column">
         <h3 class="card-title flex-fill" id="translate_question">Special title treatment</h3>
         <div class="card-text" id="translate_answer"></div>
+        <div class="progress mt-3" role="progressbar" aria-label="Example 30px high" aria-valuenow="0" aria-valuemin="0"
+            aria-valuemax="100" style="height: 30px">
+            <div id="translate_progress_bar" class="progress-bar" style="width: 0%"></div>
+        </div>
     </div>
     <div class="card-footer text-body-secondary">
         <div class="">
